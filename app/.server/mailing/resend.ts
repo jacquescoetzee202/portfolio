92e5c334b956b0fc991d5f610da263a93e496b26
Jacques <jacquescoetzee91@gmail.com>
2024-10-01 10:01:58 +1300

    escape harmful html form form input

--- app/.server/mailing/resend.ts
+++ app/.server/mailing/resend.ts
@@ -1,29 +1,32 @@
-import { Resend } from 'resend';
-import { formSubmission } from '../../types/contact';
+import { Resend } from "resend";
+import { formSubmission } from "../../types/contact";
 import type { ReactNode } from "react";
+import escape from "validator/lib/escape";
 
-import { EmailTemplate } from './EmailTemplate';
+import { EmailTemplate } from "./EmailTemplate";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-const fromAddress = process.env.RESEND_FROM_ADDRESS ?? 'Acme <onboarding@resend.dev>';
-const toAddress = process.env.RESEND_TO_ADDRESS ?? 'delivered@resend.dev';
+const fromAddress =
+  process.env.RESEND_FROM_ADDRESS ?? "Acme <onboarding@resend.dev>";
+const toAddress = process.env.RESEND_TO_ADDRESS ?? "delivered@resend.dev";
 
-/** TODO
- * - sanitize message for html template 'express-validator'
- * - escape any html characters put in by user 'he' npm package
-**/
-  
-export async function sendContactMessage({name, message} : Required<formSubmission>){
-    const { data, error } = await resend.emails.send({
-        from: fromAddress,
-        to: [toAddress],
-        subject: `Portfolio site contact: ${name}`,
-        react: EmailTemplate({name,message}),
-    });
+export async function sendContactMessage({
+  name,
+  message,
+}: Required<formSubmission>) {
+  const escapedMessage = escape(message);
+  const escapedName = escape(name);
 
-    if (error) {
-        return console.error({ error });
-    }
-    
-    console.log({ data });
+  const { data, error } = await resend.emails.send({
+    from: fromAddress,
+    to: [toAddress],
+    subject: `Portfolio site contact: ${escapedName}`,
+    react: EmailTemplate({ name: escapedName, message: escapedMessage }),
+  });
+
+  if (error) {
+    return console.error({ error });
+  }
+
+  console.log(`Email succesfully sent, id: ${data?.id}`);
 }
