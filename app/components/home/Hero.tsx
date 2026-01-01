87a029cf7ade53a528dcadc075bf80f13ac13973
jacquescoetzee202 <jacquescoetzee91@gmail.com>
2026-01-01 14:51:41 +1300

    feat: hero sectiont text

--- app/components/home/Hero.tsx
+++ app/components/home/Hero.tsx
@@ -2,20 +2,35 @@ import StockButton from "../StockButton";
 import profileImage from "../../assets/jc_black_and_white.jpeg";
 
 export default function Hero() {
-    return (
-        <div className="mx-auto max-w-7xl px-6 lg:px-8">
-            <div className="mx-auto max-w-2xl lg:text-center">
-                <h2 className="text-base/7 font-semibold text-indigo-600">Full-Stack Developer</h2>
-                <p className="mt-2 text-pretty text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl lg:text-balance">Jacques Coetzee</p>
-                <p className="mt-6 text-lg/8 text-gray-600">Team-oriented developer crafting scalable, user-focused solutions built to last.</p>
-                <div className="relative mt-6 mx-auto w-64 h-64">
-                    <img src={profileImage} alt="Profile Image" className="rounded-full w-full h-full object-cover border-4 border-white absolute top-1 left-1 shadow-lg" />
-                </div>
-            </div>
-            <div className="mt-8 flex justify-center space-x-4 max-w-xl mx-auto">
-                <StockButton to="/contact" className="flex-1" variant="primary">Contact</StockButton>
-                <StockButton to="/about" className="flex-1" variant="secondary">About</StockButton>
-            </div>
+  return (
+    <div className="mx-auto max-w-7xl px-6 lg:px-8">
+      <div className="mx-auto max-w-2xl lg:text-center">
+        <h2 className="text-base/7 font-semibold text-indigo-600">
+          Full-Stack Developer
+        </h2>
+        <p className="mt-2 text-pretty text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl lg:text-balance">
+          Jacques Coetzee
+        </p>
+        <p className="mt-6 text-lg/8 text-gray-600">
+          Team-oriented engineer crafting scalable, robust solutions built for
+          todays user and next years developer.
+        </p>
+        <div className="relative mt-6 mx-auto w-64 h-64">
+          <img
+            src={profileImage}
+            alt="Profile Image"
+            className="rounded-full w-full h-full object-cover border-4 border-white absolute top-1 left-1 shadow-lg"
+          />
         </div>
-    )
-};
+      </div>
+      <div className="mt-8 flex justify-center space-x-4 max-w-xl mx-auto">
+        <StockButton to="/contact" className="flex-1" variant="primary">
+          Contact
+        </StockButton>
+        <StockButton to="/about" className="flex-1" variant="secondary">
+          About
+        </StockButton>
+      </div>
+    </div>
+  );
+}
